384cf5a00233e3a70488b487c5e9ac91b2abf18e
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-09 02:26:43 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,7 +1,11 @@
 def first_challenge
    epic_tragedy = {
      :montague =>
-     {},
+     {:patriarch
+      :matriarch
+      :hero 
+      :hero_friends
+     },
      :capulet =>
      {}
    }
